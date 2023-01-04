4cf58270c2a93d4203f3e69b6c016da6059ffee2
W4tz0w <giovannoni.enzo@gmail.com>
2023-01-04 19:27:39 +0100

    on attaque

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daddy_cool <daddy_cool@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/26 17:45:03 by daddy_cool        #+#    #+#             */
+/*   Updated: 2023/01/04 19:10:05 by daddy_cool       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <stddef.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 11
+
+int	ft_strlen(const char *s)
+
+#endif
